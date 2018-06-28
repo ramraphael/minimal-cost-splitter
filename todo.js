9574818a1646a90be634e07e5e9ec9f1fdc9f7fb
Raphael Ram <ramraphael@gmail.com>
2018-06-28 11:59:25 -0400

    Revised for final presentation

--- todo.js
+++ todo.js
@@ -1,21 +1,29 @@
-Round up individual money owed to one decimal place
-Total bill (with tip) on final screen header
+// Round up individual money owed to one decimal place
+// Total bill (with tip) on final screen header
+// Option to just enter the number of guests
 Quick tip entry (Average, Good, Great)
+
 Individual order for party members
+
+
+recalculateMoney(){
+
+}
+
 
 // Edit button onclick:
     // Change bill/percent contenteditable=true
     // Change focus to number
     // Hide all other edit buttons
     // Replace current edit with save & cancel
-        On saveclick:
+        // On saveclick:
             // Leave focus
-            Change appropriate variable to element value
-            Update all other partymember bills
-            Update table fields
+            // Change appropriate variable to element value
+            // Update all other partymember bills
+            // Update table fields
             // Hide save and cancel, show edit
-        On cancel:
-            Undo
+        // On cancel:
+            // Undo
             // Leave focus
             // Hide save and cancel, show edit
 
